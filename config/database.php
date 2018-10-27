48eed23f220b39cbc03ed3f68dcf6be8e1827e10
Jordan Meta <irnawatyully@gmail.com>
2018-10-27 16:12:19 +0800

    Change db connection

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', 'http://127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'gracevetdb'),
-            'username' => env('DB_USERNAME', 'gracevetdb'),
-            'password' => env('DB_PASSWORD', 'gracevetdb'),
+            'database' => env('DB_DATABASE', 'gracevet-db'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
